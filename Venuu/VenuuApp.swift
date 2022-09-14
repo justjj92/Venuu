1ce5211a8d66041b77a66a6a50431b6167225738
Justin Jordan <justin.jordan92@gmail.com>
2022-09-13 23:13:01 -0700

    Initial Commit

---
+++ Venuu/VenuuApp.swift
@@ -0,0 +1,17 @@
+//
+//  VenuuApp.swift
+//  Venuu
+//
+//  Created by J J on 9/13/22.
+//
+
+import SwiftUI
+
+@main
+struct VenuuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
